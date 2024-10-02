1d16693c90e49590b283104fc8f549177b94d6b4
niharikak <niharika.kallem@empover.com>
2024-10-02 19:03:08 +0530

    added dashboard logo

--- components/layout/sidebar.tsx
+++ components/layout/sidebar.tsx
@@ -1,11 +1,14 @@
 'use client';
 import React, { useState } from 'react';
+import Image from 'next/image';
 import { DashboardNav } from '@/components/dashboard-nav';
 import { navItems } from '@/constants/data';
 import { cn } from '@/lib/utils';
 import { ChevronLeft } from 'lucide-react';
 import { useSidebar } from '@/hooks/useSidebar';
 import Link from 'next/link';
+import logo from '../../public/images/logo2.svg';
+
 
 type SidebarProps = {
   className?: string;
@@ -31,18 +34,7 @@ export default function Sidebar({ className }: SidebarProps) {
           href={'https://github.com/Kiranism/next-shadcn-dashboard-starter'}
           target="_blank"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="mr-2 h-6 w-6"
-          >
-            <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
-          </svg>
+          <Image src={logo} alt="logo" />
         </Link>
       </div>
       <ChevronLeft
